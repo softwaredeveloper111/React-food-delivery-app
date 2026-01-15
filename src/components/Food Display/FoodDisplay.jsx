a6bef18236d6bb2c726e554b3fd0b1acf63e7b3c
Sourav Giri <computerscienceengineer1@gmail.com>
2026-01-16 02:28:11 +0530

    second section completed

--- src/components/Food Display/FoodDisplay.jsx
+++ src/components/Food Display/FoodDisplay.jsx
@@ -1,18 +1,21 @@
 import React, { useContext } from 'react'
 import style from "./foodDisplay.module.css";
 import { storeContext } from "../../context/StoreContext";
+import FoodDisplayCard from '../FoodItemCard/FoodDisplayCard';
 
 const FoodDisplay = () => {
 
  
   const {items,setItems} = useContext(storeContext);
-  console.log(items)
+
 
   return (
     <div className={style.FoodDisplayContainer} >
 
           <h2>Top dishes near you</h2>
-
+         <div className={style.foodDisplayItemContainer}>
+          {items.map(item=><FoodDisplayCard key={crypto.randomUUID()} item={item}/>)}
+         </div>
     </div>
   )
 }
